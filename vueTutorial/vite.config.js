b190151b5ed5875c97f88495b5a821529a835e8e
noahbrase1 <noahgbrase@gmail.com>
2025-07-24 12:25:14 -0600

    initial commit

--- vueTutorial/vite.config.js
+++ vueTutorial/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // <-- Add this line for correct assets path on deployment
   plugins: [
     vue(),
     vueDevTools(),
